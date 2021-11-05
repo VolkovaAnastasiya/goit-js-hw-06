a627434a232699c77775042d99e86aee0ca785a9
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-11-05 20:01:44 +0200

    Изменяет 9 дз

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,29 @@
-
-const bodyEl = document.body;
-const buttonEl = document.querySelector('.change-color')
-
+const colorEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
 
 
 const getRandomHexColor = () => {
- return bodyEl.style.backgroundColor =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ return colorEl.style.color =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 buttonEl.addEventListener("click",  getRandomHexColor);
+
+
+
+
+
+
+
+
+
+
+// const bodyEl = document.body;
+// const buttonEl = document.querySelector('.change-color')
+
+
+
+// const getRandomHexColor = () => {
+//  return bodyEl.style.backgroundColor =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// buttonEl.addEventListener("click",  getRandomHexColor);
